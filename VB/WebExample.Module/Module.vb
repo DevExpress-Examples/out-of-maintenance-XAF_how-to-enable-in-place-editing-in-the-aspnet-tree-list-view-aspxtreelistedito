f422fc6c307b7fd22550547e8abcbbcff05918cd
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebExample.Module/Module.vb
@@ -0,0 +1,17 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+
+Imports DevExpress.ExpressApp
+Imports System.Reflection
+
+
+Namespace WebExample.Module
+	Public NotInheritable Partial Class WebExampleModule
+		Inherits ModuleBase
+		Public Sub New()
+			ModelDifferenceResourceName = "WebExample.Module.Model.DesignedDiffs"
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
