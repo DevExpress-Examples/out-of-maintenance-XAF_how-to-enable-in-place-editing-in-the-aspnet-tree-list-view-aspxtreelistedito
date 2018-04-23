493b80d22e107868136a201bbc1dbee883494cf5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebExample.Module/Module.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+using DevExpress.ExpressApp;
+using System.Reflection;
+
+
+namespace WebExample.Module {
+    public sealed partial class WebExampleModule : ModuleBase {
+        public WebExampleModule() {
+            ModelDifferenceResourceName = "WebExample.Module.Model.DesignedDiffs";
+            InitializeComponent();
+        }
+    }
+}
